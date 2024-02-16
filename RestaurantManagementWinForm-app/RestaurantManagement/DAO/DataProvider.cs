d245196a95f07a24cbdd33c67d0574699f037222
Văn Nam <vannamdang2003@gmail.com>
2024-02-17 00:48:43 +0700

    id

---
+++ RestaurantManagementWinForm-app/RestaurantManagement/DAO/DataProvider.cs
@@ -0,0 +1,124 @@
+﻿
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace RestaurantManagement.DAO
+{
+    public class DataProvider
+    {
+        private static DataProvider instance;
+
+        public static DataProvider Instance 
+        {
+            get
+            { 
+                if (instance == null) instance = new DataProvider();
+                return DataProvider.instance;
+            } 
+            set => DataProvider.instance = value; 
+        }
+
+        private DataProvider() { }
+
+        //private string connectionString = System.Configuration.ConfigurationSettings.AppSettings["MyConnectString"].ToString();
+        //private string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["MyConnectString"].ConnectionString;
+        private string connectionString = "data source=VAN-NAM;initial catalog=restaurant;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
+
+
+        public DataTable ExecuteQuery(string query, object[] parameter = null)
+        {
+            DataTable data = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] listParams = query.Split(' ');
+                    int i = 0;
+                    foreach (string param in listParams)
+                    {
+                        if (param.Contains("@"))
+                        {
+                            command.Parameters.AddWithValue(param, parameter[i]);
+                            i++;    
+                        }
+                    }
+                }
+                
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                adapter.Fill(data);
+
+                connection.Close();
+            }
+
+            return data;
+        }
+
+        public int ExecuteNonQuery(string query, object[] parameter = null)
+        {
+            int data = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] listParams = query.Split(' ');
+                    int i = 0;
+                    foreach (string param in listParams)
+                    {
+                        if (param.Contains("@"))
+                        {
+                            command.Parameters.AddWithValue(param, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                
+                data = command.ExecuteNonQuery();
+                connection.Close();
+            }
+
+            return data;
+        }
+
+        public object ExecuteScalar(string query, object[] parameter = null)
+        {
+            object data = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] listParams = query.Split(' ');
+                    int i = 0;
+                    foreach (string param in listParams)
+                    {
+                        if (param.Contains("@"))
+                        {
+                            command.Parameters.AddWithValue(param, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                data = command.ExecuteScalar();
+                connection.Close();
+            }
+
+            return data;
+        }
+    }
+}
